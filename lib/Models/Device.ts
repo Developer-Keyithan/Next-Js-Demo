1a5aaff5d5245e7784b48fc40da518a3a69f666a
Developer-Keyithan <sathyjaseelankeyithan@gmail.com>
2025-01-07 15:04:18 +0530

    PATCH method pending

--- lib/Models/Device.ts
+++ lib/Models/Device.ts
@@ -1,10 +1,11 @@
 import { Schema, model, models } from "mongoose";
 
 const deviceSchema = new Schema({
-    name: { type: String, required: true },
-    price: { type: String, required: true },
-    category: { type: String, required: true }
-},{ timestamps: true });
+
+    device: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true }
+}, { timestamps: true });
 
 const deviceModel = models.Device || model('Device', deviceSchema);
 
---
+++ src/app/api/devices/route.ts
@@ -0,0 +1,70 @@
+import { NextResponse } from 'next/server';
+import DBconnect from '../../../../lib/db';
+import Device from '../../../../lib/Models/Device';
+
+export const GET = async () => {
+    try {
+        await DBconnect();
+
+        const body = await Device.find();
+
+        return NextResponse.json(body, { status: 200 });
+    } catch (error) {
+        console.error("Error fetching body:", error);
+
+        return NextResponse.json({ message: "Failed to fetch body." }, { status: 500 });
+    }
+};
+
+export const POST = async (req: Request) => {
+    try {
+        await DBconnect();
+
+        const newDevice = await req.json();
+        console.log("Device to save:", newDevice);
+
+        const savedDevice = await Device.insertMany(newDevice);
+
+        return NextResponse.json({ savedDevice },{ status: 200 });
+    } catch (error: any) {
+        console.error("Error saving body:", error.message);
+        return NextResponse.json(
+            { message: "Failed to save body.", error: error.message },
+            { status: 500 }
+        );
+    }
+};
+
+
+// export const PATCH = async (req: Request, res: NextResponse) {
+//     try {
+//         await DBconnect();
+
+
+
+//         const update = await req.json();
+//         console.log("Device to update:", update);
+
+//         const updatedDevice = await Device.findByIdAndUpdate(update);
+
+//         return NextResponse.json({ updatedDevice }, { status: 201 });
+//     } catch (error: any) {
+//         return NextResponse.json({ message: "Failed to fetch body." }, { status: 500 });
+//     }
+// }
+
+export const DELETE = async (req: Request, res: NextResponse) {
+    try {
+        await DBconnect();
+
+        // const { id } = req.body.id
+
+        const deleteDevice = '';
+
+        const deletedDevice = await Device.findByIdAndDelete(deleteDevice);
+
+        return NextResponse.json({ deletedDevice }, { status: 200 })
+    } catch (error) {
+        
+    }
+}
